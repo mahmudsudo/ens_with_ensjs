7843f4f96c86e289f02c1a810642c7b766fd62b9
mahmud bello <blinkztyler@gmail.com>
2023-09-26 02:51:05 +0100

    blockers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,122 @@
 import Head from 'next/head'
 import { useState, useEffect, FormEvent } from "react"
 import { Inter } from 'next/font/google'
-import {ethers} from "ethers"
-import ENS ,{getEnsAddress} from "@ensdomains/ensjs"
+import {Eip1193Provider, ethers} from "ethers"
+import { ENS } from "@ensdomains/ensjs";
 import styles from '@/styles/Home.module.css'
 
-
-// const [provider, setProvider] = useState<Provider | null>(null);
-//   const [signer, setSigner] = useState<ethers.Signer | null>(null);
-//   const [address, setAddress] = useState<string | null>(null);
-//   const [network, setNetwork] = useState<Network | null>(null);
-//   const [blockNum, setBlockNum] = useState<number | null>(null);
-  // useEffect(() => {
-  //   // connect if we already have metamask approval
-  //   try {
-  //     checkAndConnect();
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  //   // eslint-disable-next-line
-  // }, []);
-
 const inter = Inter({ subsets: ['latin'] })
-type Provider = ethers.providers.Web3Provider;
-type Network = ethers.providers.Network;
-
 declare global {
   interface Window {
-    ethereum: any | undefined;
+    ethereum: any;
   }
 }
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-  const signer = await provider.getSigner();
-  const address = await signer.getAddress();
-  const network = await provider.getNetwork();
 
-  // make sure page refreshes when network is changed
+
+
+export default  function Home() {
+  const [connected,setConnected] = useState(false)
+  const [name,setName] = useState(" ");
+  
+type Provider = ethers.Provider;
+type Network = ethers.Network;
+let providers :ethers.BrowserProvider
+if (typeof window !== "undefined") {
+   providers = new ethers.BrowserProvider(window.ethereum);
+    providers.send("eth_requestAccounts", []);
+   let signer  : ethers.Signer
+   let address :string;   
+   providers.getSigner().then(el => signer =el).then(el => el.getAddress()).then(el => address =el).then(el => console.log(el));
+   const provider = new ethers.JsonRpcProvider("homestead")
+
+const ENSInstance = new ENS()
+ ENSInstance.withProvider(provider)
+   
+   const network = provider.getNetwork();
+   // make sure page refreshes when network is changed
   // https://github.com/MetaMask/metamask-extension/issues/8226
   window.ethereum.on("chainIdChanged", () => window.location.reload());
   window.ethereum.on("chainChanged", () => window.location.reload());
 
-const ens = new ENS({ provider, ensAddress: getEnsAddress('1') });
-const label = ens.name("tife.eth")
-
-const attemptConnection = async () => {
-  if (window.ethereum === undefined) {
-    throw Error("MetaMask not found, please visit https://metamask.io/");
-  }
-
-  // get provider, address, and network
-  
-
-  // set states
-  // setSigner(signer);
-  // setProvider(provider);
-  // setAddress(address);
-  // setNetwork(network);
-};
-
-const connect = async () => {
-  try {
-    await attemptConnection();
-    window.ethereum.on("accountsChanged", () => attemptConnection());
-  } catch (error :any) {
-    console.error(error);
-    alert(error.message);
-  }
-};
-
-const checkAndConnect = async () => {
-  if (window?.ethereum?.request) {
-    const availableAccounts = await window.ethereum.request({
-      method: "eth_accounts",
-    });
-    if (availableAccounts.length > 0) {
-      connect();
+  const attemptConnection = async () => {
+    if (window.ethereum === undefined) {
+      throw Error("MetaMask not found, please visit https://metamask.io/");
     }
-  }
-};
-
-
-
-
-export default function Home() {
-  const [connected,setConnected] = useState(false)
-  const [name,setName] = useState(" ");
   
-const HandleOnclick = ( ) => {
-  connect();
-  setConnected(true);
-}
-  async function HandleSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
-    event.preventDefault();
-    await label.createSubDomain(name);
+  };
 
+
+  const connect = async () => {
+    try {
+     attemptConnection();
+      window.ethereum.on("accountsChanged", () => attemptConnection());
+    } catch (error :any) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+  const checkAndConnect = async () => {
+    if (window?.ethereum?.request) {
+      const availableAccounts = window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (availableAccounts.length > 0) {
+        connect();
+      }
+    }
+  };
+  
+    
+  const HandleOnclick = ( ) => {
+    connect();
+    setConnected(true);
   }
+    async function HandleSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
+      event.preventDefault();
+  
+      
+      const label = ens.name("trace")
+      console.log(label);
+      
+     label.createSubDomain(name);
+  
+    }
+  
+    return (
+      <>
+        <Head>
+          <title>Create Next App</title>
+          
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={`${styles.main} ${inter.className}`}>
+         <button onClick={HandleOnclick}>
+          { connected ?
+          "connected"
+          :
+          "connect wallet"  }
+         </button>
+            <form onSubmit={HandleSubmit}>
+              <input type="text" name="name" id="" onChange={(e: { target: { value: any } }) => {
+          setName(e.target.value)
+        }}/>
+              <input type="submit" value="submit" />
+            </form>
+  
+        </main>
+      </>
+    )
 
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-       <button onClick={HandleOnclick}>
-        { connected ?
-        "connected"
-        :
-        "connect wallet"  }
-       </button>
-          <form onSubmit={HandleSubmit}>
-            <input type="text" name="name" id="" onChange={(e: { target: { value: any } }) => {
-        setName(e.target.value)
-      }}/>
-            <input type="submit" value="submit" />
-          </form>
+}
 
-      </main>
-    </>
-  )
+
+ 
+
+  
+
+
+
+
+
+
 }
